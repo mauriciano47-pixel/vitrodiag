a7b1ea97a79cfd6a8b5122eda28a54397d355688
mauriciano47-pixel <mauriciano47-pixel@users.noreply.github.com>
2026-07-10 18:13:49 -0400

    Motor IA: Integracion de biblioteca TensorFlow.js, cargador asincrono de modelos CNN y registro offline

--- sw.js
+++ sw.js
@@ -2,6 +2,7 @@ const CACHE_NAME = 'vitrodiag-cache-v19';
 const ASSETS = [
   './index.html',
   './manifest.json',
+  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.20.0/dist/tf.min.js',
   'https://img.icons8.com/neon/96/glass-bottle.png',
   'https://img.icons8.com/neon/192/glass-bottle.png',
   'https://img.icons8.com/neon/512/glass-bottle.png'
